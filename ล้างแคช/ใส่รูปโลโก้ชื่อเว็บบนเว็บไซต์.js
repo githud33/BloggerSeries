684b074507d4bda3b91c7f21ad35b37814f4142a
githud33 <githud33@gmail.com>
2026-06-07 20:24:25 +0700

    Add files via upload

---
+++ ล้างแคช/ใส่รูปโลโก้ชื่อเว็บบนเว็บไซต์.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/BloggerSeries/ใส่รูปโลโก้ชื่อเว็บบนเว็บไซต์.js?t=' + new Date().getTime();
+
+document.head.appendChild(script);
