747ff166c4ffba28f9285486a9a3eb376629b509
githud33 <githud33@gmail.com>
2026-06-09 16:58:20 +0700

    Update ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js

--- ล้างแคช/ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js
+++ ล้างแคช/ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 // ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
 script.src = 'https://githud33.github.io/BloggerSeries/ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js?t=' + new Date().getTime();
-script.defer = true;
+
 document.head.appendChild(script);
