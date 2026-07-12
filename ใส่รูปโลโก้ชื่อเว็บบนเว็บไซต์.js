08868e3aa40ad2cb0a2b82628cd9d50c137dadab
githud33 <githud33@gmail.com>
2026-07-12 18:24:27 +0700

    Update ใส่รูปโลโก้ชื่อเว็บบนเว็บไซต์.js

--- ใส่รูปโลโก้ชื่อเว็บบนเว็บไซต์.js
+++ ใส่รูปโลโก้ชื่อเว็บบนเว็บไซต์.js
@@ -1,7 +1,8 @@
 
-        var myImage = document.getElementById('logo-1'); // สมมติว่ามี ID ชื่อ 'logo-1'
-        myImage.src = 'https://mey2015.github.io/image/uploads/logo250-A.png'; // เปลี่ยน URL รูปภาพ
-         // https://raw.githubusercontent.com/mey2015/image/main/uploads/logo250-A.png
-        // https://dl.dropboxusercontent.com/scl/fi/2qhk98jcbzou8nn40y08w/logo250-A.png?rlkey=gaf2bneklx7tmqotzvgk24dpc
-        // https://dl.dropboxusercontent.com/scl/fi/qjhjr5g7cvxhzgbznvx6o/logoอาลัย.png?rlkey=aofax3pn4mj91qdzhlbfst6eg
+var myImage = document.getElementById('logo-1'); // สมมติว่ามี ID ชื่อ 'logo-1'
+myImage.src = 'https://cdn.jsdelivr.net/gh/mey2015/image@main/uploads/logo250-A.png'; // เปลี่ยน URL รูปภาพ
+// //  พิมพ์ purge แทนตัว cdn เพื่อล้างแค้น https://purge.jsdelivr.net/
+// https://raw.githubusercontent.com/mey2015/image/main/uploads/logo250-A.png
+// https://dl.dropboxusercontent.com/scl/fi/2qhk98jcbzou8nn40y08w/logo250-A.png?rlkey=gaf2bneklx7tmqotzvgk24dpc
+// https://dl.dropboxusercontent.com/scl/fi/qjhjr5g7cvxhzgbznvx6o/logoอาลัย.png?rlkey=aofax3pn4mj91qdzhlbfst6eg
 
