0eb1b6535ff92582b0d3fc3db9b20125c50362f1
githud33 <githud33@gmail.com>
2026-07-12 18:21:08 +0700

    Update ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js

--- ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js
+++ ใส่รูปข้างหลัง𝐆𝐨𝐨𝐝𝐒𝐞𝐫𝐢𝐞𝐬.js
@@ -1,5 +1,6 @@
-        var myImage = document.getElementById('myImg-1'); // สมมติว่ามี ID ชื่อ 'myImg-1'
-        myImage.src = 'https://cdn.jsdelivr.net/gh/mey2015/image@main/uploads/รูปโลโก้แดง.png'; 
-         //  https://dl.dropboxusercontent.com/scl/fi/r9d79nujb8dz2nl9eyk4h/รูปโลโก้แดง.png?rlkey=2gyc9xmsl8djocievp5ckwphj
-        // เปลี่ยน URL รูปภาพ  https://dl.dropboxusercontent.com/scl/fi/r9d79nujb8dz2nl9eyk4h/รูปโลโก้แดง.png?rlkey=2gyc9xmsl8djocievp5ckwphj
-       // https://dl.dropboxusercontent.com/scl/fi/vbu9z9c0gv6cdrjdw4dcp/สัญลักษณ์ไว้อาลัย.png?rlkey=03y618x8sfrq3bzoh0yjwvmdo
+var myImage = document.getElementById('myImg-1'); // สมมติว่ามี ID ชื่อ 'myImg-1'
+myImage.src = 'https://cdn.jsdelivr.net/gh/mey2015/image@main/uploads/รูปโลโก้แดง.png'; 
+//  พิมพ์ purge แทนตัว cdn เพื่อล้างแค้น https://purge.jsdelivr.net/
+//  https://dl.dropboxusercontent.com/scl/fi/r9d79nujb8dz2nl9eyk4h/รูปโลโก้แดง.png?rlkey=2gyc9xmsl8djocievp5ckwphj
+// เปลี่ยน URL รูปภาพ  https://dl.dropboxusercontent.com/scl/fi/r9d79nujb8dz2nl9eyk4h/รูปโลโก้แดง.png?rlkey=2gyc9xmsl8djocievp5ckwphj
+// https://dl.dropboxusercontent.com/scl/fi/vbu9z9c0gv6cdrjdw4dcp/สัญลักษณ์ไว้อาลัย.png?rlkey=03y618x8sfrq3bzoh0yjwvmdo
